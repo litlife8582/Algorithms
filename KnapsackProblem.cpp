069603f83557179ab7f187e966747a71f811faad
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2026-02-21 01:09:48 +0530

    Knapsack Updated

--- KnapsackProblem.cpp
+++ KnapsackProblem.cpp
@@ -1,47 +1,49 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 
 int main() {
-    int n,limit;
+    double n;
     cin>>n;
+    vector<vector<double>> A(n,vector<double>(3));
 
-    double arr[100][3];
-    for(int i=0;i<n;i++) cin>>arr[i][0];
-    for(int i=0;i<n;i++) cin>>arr[i][1];
+    for (int i=0;i<n;i++) cin>>A[i][0];
+    for (int i=0;i<n;i++) cin>>A[i][1];
+    for (int i=0;i<n;i++) A[i][2]=A[i][1]/A[i][0];
 
+    double limit;
     cin>>limit;
 
     for (int i=0;i<n;i++) {
-        arr[i][2]=arr[i][1]/arr[i][0];
-    }
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j][2]<arr[j+1][2]){
-                for (int k=0;k<3;k++) swap(arr[j][k],arr[j+1][k]);
+        for (int j=0;j<n-i-1;j++) {
+            if (A[j][2]<A[j+1][2]) {
+                double temp0=A[j][0];
+                double temp1=A[j][1];
+                double temp2=A[j][2];
+                A[j][0]=A[j+1][0];
+                A[j][1]=A[j+1][1];
+                A[j][2]=A[j+1][2];
+                A[j+1][0]=temp0;
+                A[j+1][1]=temp1;
+                A[j+1][2]=temp2;
             }
         }
     }
 
-    double rw=limit;
-    double tw,tv;
-    double cw=0;
+    double totalValue=0,totalWeight=0;
+
     for (int i=0;i<n;i++) {
-        if (cw+arr[i][0]<=limit) {
-            cw+=arr[i][0];
-            tv+=arr[i][1];
-            cout<<arr[i][0]<<" "<<arr[i][1]<<endl;
-        }else {
-            double rw=limit-cw;
-            tv+=arr[i][1]*(rw/arr[i][0]);
-            cout<<rw<<" "<<arr[i][1]*(rw/arr[i][0])<<endl;
-            break;
+        if (totalWeight+A[i][0]<=limit) {
+            totalWeight+=A[i][0];
+            totalValue+=A[i][1];
+        }else if(totalWeight+A[i][0]>limit){
+            double remainingWeight=limit-totalWeight;
+            totalValue+=A[i][2]*remainingWeight;
+            totalWeight+=remainingWeight;
         }
     }
 
-    cout<<"Total Profit: "<<tv<<endl;
 
+    cout<<"Total profit: "<<totalValue<<endl;
     return 0;
 }
